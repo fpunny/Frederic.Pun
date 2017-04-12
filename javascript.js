0ac153ff9045c53cbe21587610c3e403ce8b6dca
Frederic Pun <frederic.pun@gmail.com>
2017-04-11 20:29:35 -0400

    Fixed stuff
    More stuff

--- javascript.js
+++ javascript.js
@@ -3,10 +3,10 @@ var dropped = false;
 function togglemenu() {
     var menu = document.getElementById('nav');
     if(dropped === false) {
-        menu.style.top = '0px';
+        menu.style.top = '0vh';
         dropped = true;
     } else {
-        menu.style.top = '-170px';
+        menu.style.top = '-23.5vh';
         dropped = false;
     }
 }
